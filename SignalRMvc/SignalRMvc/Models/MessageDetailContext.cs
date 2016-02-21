6b1e2d147ecb66257cd08d8ae9650c6653019f51
alex2880s <alexandershnip94@gmail.com>
2016-02-21 21:38:14 +0300

    .

---
+++ SignalRMvc/SignalRMvc/Models/MessageDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace SignalRMvc.Models
+{
+    public class MessageDetailContext:DbContext
+    {
+        public DbSet<MessageDetail> MessageDetails { get; set; }
+
+    }
+}
